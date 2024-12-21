40e9c127dfaf88324317441a942b2318bbede4bf
Ahmed <sanada927@gmail.com>
2024-12-21 21:24:15 +0200

    Add project files.

---
+++ LeetCodeSolvingInC#/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LeetCodeSolvingInC_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Here We will do prolem solving In C#");
+        }
+    }
+}
