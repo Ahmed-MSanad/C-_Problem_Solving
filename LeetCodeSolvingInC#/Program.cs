e3d3febc6bca9a7c8a5557c60fd02c6013940765
Ahmed <sanada927@gmail.com>
2025-01-30 17:30:40 +0200

    Solving_RemoveElement_problem_https://leetcode.com/problems/remove-element/description/?envType=study-plan-v2&envId=top-interview-150

--- LeetCodeSolvingInC#/Program.cs
+++ LeetCodeSolvingInC#/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Here We will do prolem solving In C#");
+            Console.WriteLine($"{RemoveElement([0, 1, 2, 2, 3, 0, 4, 2], 2)}"); // answer: 5 remaining numbers than the Val = 2
         }
+
+        public static int RemoveElement(int[] nums, int val)
+        {
+            List<int> notValNumbers = new List<int>();
+            foreach (int num in nums)
+                if (num != val)
+                    notValNumbers.Add(num);
+            for (int i = 0; i < notValNumbers.Count; i++)
+            {
+                nums[i] = notValNumbers[i];
+            }
+
+            return notValNumbers.Count;
+        }
+
     }
 }
