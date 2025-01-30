afc315804c8dd5a7c7f16c6040c8904e266402d1
Ahmed <sanada927@gmail.com>
2025-01-30 18:51:47 +0200

    Solving_RemoveDuplicates_problem_https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/?envType=study-plan-v2&envId=top-interview-150

--- LeetCodeSolvingInC#/Program.cs
+++ LeetCodeSolvingInC#/Program.cs
@@ -4,7 +4,65 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Here We will do prolem solving In C#");
+            Console.WriteLine($"{RemoveDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4])}");
+            Console.WriteLine($"{RemoveDuplicates([1, 1, 2])}");
+
+            Console.WriteLine($"{RemoveDuplicates_solution_2([0, 0, 1, 1, 1, 2, 2, 3, 3, 4])}");
+            Console.WriteLine($"{RemoveDuplicates_solution_2([1, 1, 2])}");
+
         }
+        public static int RemoveDuplicates(int[] nums)
+        {
+            Dictionary<int, int> number_index = new Dictionary<int, int>();
+
+            int current_idx = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                //if (!number_index.TryGetValue(nums[i], out int index))
+                //{
+                //    index = current_idx++;
+                //    number_index[nums[i]] = index;
+                //}
+
+                // OR:
+
+                //if (!number_index.ContainsKey(nums[i]))
+                //{
+                //    number_index[nums[i]] = current_idx++;
+                //}
+
+                // OR:
+
+                number_index[nums[i]] = number_index.GetValueOrDefault(nums[i], current_idx++);
+            }
+
+            foreach (var pair in number_index)
+            {
+                nums[pair.Value] = pair.Key;
+            }
+
+            return number_index.Count;
+        }
+
+        public static int RemoveDuplicates_solution_2(int[] nums)
+        {
+            // solving using 2-pointers tech
+            int p1 = 0, p2 = 1; // nums => [1, 1, 2, 3] => [1, 2, 1, 3] => [1, 2, 3, 1]
+            while (p1 + 1 < nums.Length && p2 < nums.Length)
+            {
+                if (nums[p1] != nums[p2])
+                {
+                    (nums[p1 + 1], nums[p2]) = (nums[p2], nums[p1 + 1]);
+                    p1++;
+                    p2++;
+                }
+                else
+                {
+                    p2++;
+                }
+            }
+            return p1 + 1;
+        }
+
     }
 }
